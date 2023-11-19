81fb5c42f52477f680fd4f6fbd4736521591cb9a
Renato Athaydes <renato@athaydes.com>
2023-11-19 10:12:17 +0100

    Updated example.

--- example/schemake_example.dart
+++ example/schemake_example.dart
@@ -1,7 +1,8 @@
 import 'package:schemake/schemake.dart';
+import 'package:schemake/src/dart_gen.dart';
 import 'package:yaml/yaml.dart';
 
-const person = Objects('Person', {
+const personSchema = Objects('Person', {
   'name': Property<String>(type: Strings()),
   'age': Property<int?>(type: Nullable(Ints())),
 });
@@ -11,7 +12,14 @@ void main() {
   name: Joe Doe
   age: 42
   ''');
-  final joe = person.convertToDart(yaml);
+  final joe = personSchema.convertToDart(yaml);
   print('Full name: ${joe['name']}');
   print('Age:       ${joe['age']}');
+
+  // generate a Dart class from the schema... prints:
+  // class Person {
+  //   String name;
+  //   int? age;
+  // }
+  print(generateDart([personSchema]));
 }
